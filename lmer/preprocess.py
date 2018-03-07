958aaed423abe014106bc502b1bb6b9a867934fe
wessonmo <wessonmo@gmail.com>
2018-03-07 09:48:29 -0600

    update

--- lmer/preprocess.py
+++ lmer/preprocess.py
@@ -16,7 +16,10 @@ school_info = pd.merge(school_info, cities, how = 'left', on = 'city')\
                       'longitude': 'arena_long'})
 
 games = pd.read_csv('csv\\games.csv', header = 0)
-games = games.loc[pd.isnull(games.opp_id) == False]
+games = games.loc[(pd.isnull(games.opp_id) == False) & (games.school_id == games[['school_id','opp_id']].min(axis = 1))]
+games.loc[:,'game_id'] = games.apply(lambda x: re.sub('/','',x.game_date) + str(int(x.school_id)) + str(int(x.opp_id)), axis = 1)
+games.drop_duplicates(['game_id']).reset_index(drop = True).sort_values(['season','game_id'], inplace = True)
+games.loc[:,'ot'] = games.loc[:,'ot'].fillna(0)
 
 data = games
 match_cols = ['season','school_id']
@@ -40,28 +43,52 @@ for var in ['lat','long']:
         
     data.loc[:,'site_' + var] = data.apply(lambda x: x['school_' + var] if x.location == 'Home' else x['opp_' + var]
                                     if x.location == 'Away' else x['site_' + var], axis = 1)
-    
+
+distances = pd.DataFrame(columns = ['school_lat','school_long','site_lat','site_long'])
+for side, loc in zip(['school','opp'],['Home','Away']):
+    var_list = [side + '_lat', side + '_long', 'site_lat','site_long']
+    temp = data.loc[(data[var_list].isnull().sum(axis = 1) == 0) & (data.location != loc),var_list]
+    if side == 'opp':
+        temp.rename(index = str, columns = {'opp_lat': 'school_lat', 'opp_long': 'school_long'}, inplace = True)
+    distances = pd.concat([distances,temp]).drop_duplicates().reset_index(drop = True)
+
+distances.loc[:,'geo_dist'] = distances.apply(lambda x: vincenty((x.school_lat,x.school_long), (x.site_lat, x.site_long)).miles, axis = 1)
+
 for side in ['school','opp']:
-    data.loc[:,side + '_dist'] = data.apply(lambda x: vincenty((x[side + '_lat'], x[side + '_long']), (x.site_lat, x.site_long)).miles
-                if x[[side + '_lat', side + '_lat', 'site_lat', 'site_long']].isnull().sum() == 0 else None, axis = 1)
-            
+    temp = distances.rename(index = str, columns = {'school_lat': side + '_lat', 'school_long': side + '_long'})
+    data = pd.merge(data, temp, how = 'left', on = [side + '_lat',side + '_long', 'site_lat', 'site_long'])\
+                .rename(index = str, columns = {'geo_dist': side + '_dist'})
+    for col in ['_city','_arena','_city_lat','_city_long','_arena_lat','_arena_long','_lat','_long']:
+        data.drop([side + col], axis = 1, inplace = True)
+        
+data.drop(['site','site_lat','site_long'], axis = 1, inplace = True)
+
 data.loc[:,'perc_capacity'] = data.apply(lambda x: x.attend/x.school_capacity
                                  if (x.location == 'Home') and (x.school_capacity > 0) else
                                      x.attend/x.opp_capacity if (x.location == 'Away') and (x.opp_capacity > 0)
                                      else None, axis = 1)
 
-data.loc[:,'game_id'] = data.apply(lambda x: re.sub('/','_',x.game_date) + '.' + str(int(min(x.school_id,x.opp_id))) + '.'
-                         + str(int(max(x.school_id,x.opp_id))), axis = 1)
-
 comb = pd.DataFrame()
-column_changes = ['_'.join(x.split('_')[1:]) for x in data.columns if x.split('_')[0] == 'school']
+column_changes = ['_'.join(x.split('_')[1:]) for x in data.columns if (x.split('_')[0] == 'school')
+                    and ('_'.join(x.split('_')[1:]) != 'score')]
 for side in ['school','opp']:
-    data.loc[:,'location'] = data.apply(lambda x: 'Neutral' if x.location == 'Neutral' else x.location
+    temp = data.copy()
+    temp.loc[:,'location'] = temp.apply(lambda x: 'Neutral' if x.location == 'Neutral' else x.location
                 if side == 'school' else list(set(['Home','Away']) - set([x.location]))[0], axis = 1)
+    temp.rename(index = str, columns = {side + '_score': 'pts'}, inplace = True)
+    temp.drop([list(set(['school','opp']) - set([side]))[0] + '_score'], axis = 1, inplace = True)
     for col in column_changes:
-        data.rename(index = str, columns = {side + '_' + col: 'off_' + col}, inplace = True)
-        data.rename(index = str, columns = {list(set(['school','opp']) - set([side]))[0] + '_' + col: 'def_' + col}, inplace = True)
-        
-    comb = pd.concat([comb, data]).sort_values(['game_id','off_id'])
+        temp.rename(index = str, columns = {side + '_' + col: 'off_' + col}, inplace = True)
+        temp.rename(index = str, columns = {list(set(['school','opp']) - set([side]))[0] + '_' + col: 'def_' + col}, inplace = True)
+    
+    comb = pd.concat([comb, temp]).sort_values(['game_id','off_id']).reset_index(drop = True)
 
-comb.iloc[0]
+comb.loc[:,'ot'] = comb.loc[:,'ot'].fillna(0)
+
+for side, loc in zip(['off','def'],['Home','Away']):
+    comb.loc[:,side + '_dist'] = comb.apply(lambda x: 0 if (pd.isnull(x[side + '_dist']) == True) and (x.location == loc)
+                                    else x[side + '_dist'], axis = 1)
+    comb.loc[:,side + '_id'] = comb.apply(lambda x: str(x.season) + '_' + str(int(x[side + '_id'])), axis = 1)
+    
+with open('csv\\lmer_data.csv', 'ab') as csvfile:
+    comb.to_csv(csvfile, header = True, index = False)
