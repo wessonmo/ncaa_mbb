b0f8bd56305523991c376371f1e8b338cfcb0454
wessonmo <wessonmo@gmail.com>
2018-04-13 01:50:32 -0500

    rewrite and deprecate old code

--- ncaa_scrapers/game_ids.py
+++ ncaa_scrapers/game_ids.py
@@ -1,62 +1,72 @@
-from ncaa_scrapers.functions.soupify import soupify
-import csv
+from __future__ import print_function
+from functions.soupify import soupify
 import pandas as pd
 from collections import OrderedDict
-from time import time
+import re
+import os
+import sys
 
-school_divs = pd.read_csv('ncaa_scrapers\\csv\\school_divs.csv', header = 0)
+needed = pd.read_csv('csv\\school_index.csv', header = 0)[['season_id','school_id']]
+needed = set((row.season_id,row.school_id) for index, row in needed[['season_id','school_id']].iterrows())
 
-for season in range(2012,school_divs.season.max() + 1):
-    try:
-        game_ids = pd.read_csv('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', header = 0)
-    except IOError as error:
-        if str(error) == 'File ncaa_scrapers\csv\game_ids_' + str(season) + '.csv does not exist':
-            with open('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', 'wb') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                csvwriter.writerow(['season','school_id','game_date','opp_id','game_id'])
-            game_ids = pd.read_csv('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', header = 0)
-        else:
-            raise error
 
-    needed = pd.merge(school_divs.loc[school_divs.season == season], game_ids, how = 'left', on = ['season','school_id'],
-                      indicator = 'exist')
-    num_needed = len(needed.loc[needed.exist == 'left_only'])
+file_loc = 'csv\\game_ids.csv'
+
+exist = os.path.isfile(file_loc)
+
+scraped = set() if not exist else set((row.season_id,row.school_id) for index, row in
+              pd.read_csv(file_loc, header = 0)[['season_id','school_id']].drop_duplicates().iterrows())
+
+
+left = needed - scraped
+
+completed = len(scraped)
+for season_id, school_id in left:
     
-    i, start = 1, time()
-    for index, row in needed.loc[needed.exist == 'left_only'].sort_values('school_id').iterrows():
+    sys.stdout.flush()
+    perc_string = '%5.2f'%(float(completed)/len(needed)*100)
+    print('Game IDs: {0}% Complete'.format(perc_string), end = '\r')
+    
+    
+    url = 'http://stats.ncaa.org/team/{0}/{1}'.format(school_id, season_id)
+    
+    soup = soupify(url)
+    
+    
+    if soup.find('td', text = re.compile('Schedule\/Results')) == None:
         
-        soup = soupify('http://stats.ncaa.org/team/' + str(row.school_id) + '/' + str(row.season_id))
+        games_df = pd.DataFrame([[season_id, school_id]], columns = ['season_id','school_id'])
         
-        try:    
-            tbody = soup.find('td', text = 'Schedule/Results\n           ').parent.parent
-        except AttributeError:
-            with open('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', 'ab') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                csvwriter.writerow([row.season,row.school_id,None,None,None])
-            continue
+    else:
         
-        games = [x.find_all('td') for x in tbody.find_all('tr') if (x.get('class') == None) and (len(x.find_all('td')) == 3)]
+        games = [x for x in soup.find('td', text = re.compile('Schedule\/Results')).find_parent('table')\
+            .find_all('tr', {'class': None}) if len(x.find_all('td')) > 1]
         
         data = OrderedDict()
-        
-        data['season'] = [row.season]*len(games)
-        data['school_id'] = [row.school_id]*len(games)
-        data['game_date'] = [x[0].text for x in games]
-        data['opp_id'] = [int(x[1].find('a').get('href').split('/')[2]) if x[1].find('a') != None else None for x in games]
-        data['game_id'] = [int(x[2].find('a').get('href').split('?')[0].split('/')[-1])
-            if x[2].find('a') != None else None for x in games]
+            
+        data['season_id'] = [season_id]*len(games)
+        data['school_id'] = [school_id]*len(games)
+        data['game_date'] = [x.find('td').text for x in games]
+        data['opp_id'] = [int(x.find_all('td')[1].find('a').get('href').split('/')[2])
+            if x.find_all('td')[1].find('a') != None else None for x in games]
+        data['game_id'] = [int(x.find_all('td')[2].find('a').get('href').split('?')[0].split('/')[-1])
+            if x.find_all('td')[2].find('a') != None else None for x in games]
         
         games_df = pd.DataFrame(data)
-        games_df = games_df.loc[pd.isnull(games_df.game_id) == False]
+        games_df.loc[:,'game_date'] = pd.to_datetime(games_df.loc[:,'game_date'])
+        games_df = games_df.loc[~pd.isnull(games_df.game_id)]
         
         if len(games_df) == 0:
-            games_df.loc[0] = [row.season,row.school_id,None,None,None]
-        
-        with open('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', 'ab') as hrefscsv:
-            games_df.to_csv(hrefscsv, header = False, index = False)
+            raise ValueError('no games: {0}'.format(url))
             
-        if (i % 100 == 0) or (i == 1):
-            avg_time = (time() - start)/i
-            print(str(season) + ' game_ids Remaining: ' + str(num_needed - i) + ' ' + str(int(avg_time*(num_needed - i)/60)) + ' min')
         
-        i += 1
+    with open(file_loc, 'ab' if exist else 'wb') as csvfile:
+        games_df.to_csv(csvfile, header = not exist, index = False)
+    
+    exist = True
+    
+    
+    completed += 1
+    
+sys.stdout.flush()
+print('Game IDs: 100.00% Complete')
