6bfae1f1547619b184fcbca1260f82c6f069ea4b
wessonmo <wessonmo@gmail.com>
2018-04-13 01:50:12 -0500

    rewrite and deprecate old code

--- ncaa_scrapers/box_scores.py
+++ ncaa_scrapers/box_scores.py
@@ -1,84 +1,95 @@
-from ncaa_scrapers.functions.soupify import soupify
-import csv
+from __future__ import print_function
+from functions.soupify import soupify
 import pandas as pd
 from collections import OrderedDict
 import re
-from time import time
+import os
+import sys
 
-box_score_var = ['FGM','FGA','3FG','3FGA','FT','FTA','Pts','ORB','DRB','TRB','AST','TO','STL','BLK','FL']
+needed = pd.read_csv('csv\\game_ids.csv', header = 0)
+needed = set(needed.loc[~pd.isnull(needed.game_id)].game_id)
 
-school_divs = pd.read_csv('ncaa_scrapers\\csv\\school_divs.csv', header = 0)
 
-for season in range(2012,school_divs.season.max() + 1):
-    
-    all_game_ids = pd.read_csv('ncaa_scrapers\\csv\\game_ids_' + str(season) + '.csv', header = 0)
-    all_game_ids = set(x for x in set(all_game_ids.loc[pd.isnull(all_game_ids.game_id) == False].game_id))
-    
-    try:
-        scraped_game_ids = set(pd.read_csv('ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv', header = 0).game_id)
-    except IOError as error:
-        if str(error) == 'File ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv does not exist':
-            with open('ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv', 'wb') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                csvwriter.writerow(['game_id','period','school_id','player_order','player_href','player_name','pos','min']
-                    + box_score_var)
-            scraped_game_ids = set()
-        else:
-            raise error
+file_loc = 'csv\\box_scores.csv'
+
+exist = os.path.isfile(file_loc)
+
+scraped = set() if not exist else set(pd.read_csv(file_loc, header = 0).game_id)
+
+
+left = needed - scraped
+
+completed = len(scraped)
+for game_id in left:
             
-    game_ids_needed = sorted(all_game_ids - scraped_game_ids)
-    num_needed = len(game_ids_needed)
+    sys.stdout.flush()
+    perc_string = '%5.2f'%(float(completed)/len(needed)*100)
+    print('Box Scores: {0}% Complete'.format(perc_string), end = '\r')
     
-    i, start = 1, time()
-    for game_id in game_ids_needed:
+    
+    for period in [1,2]:
         
-        for period in [1,2]:
-            soup = soupify('http://stats.ncaa.org/game/box_score/' + str(int(game_id)) + '?period_no=' + str(period))
+        url = 'http://stats.ncaa.org/game/box_score/{0}?period_no={1}'.format(game_id, period)
+        
+        soup = soupify(url)
+        
+        teams = soup.find_all('tr', {'class': 'heading'})
+        
+        if (teams == []) and (period == 2):
+            break
             
-            school_ids = [int(x.get('href').split('/')[2]) for x in soup.find_all('a', {'href': re.compile('\/team\/[0-9]+')})]
+        elif teams == []:
             
-            box_scores = [x.parent.parent.find_all('tr', {'class': 'smtext'}) for x in soup.find_all('th', text = 'Player')]
+            box_df = pd.DataFrame([[game_id] + [None]*7],
+                              columns = ['game_id','period','school_id','order','player_id','player_name','pos','min'])
             
-            try:
-                box_scores[0]
-            except IndexError:
-                with open('ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv', 'ab') as hrefscsv:
-                    hrefwriter = csv.writer(hrefscsv, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                    hrefwriter.writerow([game_id,period] + [None]*21)
-                break
-            
-            for box_score_soup, school_id in zip(box_scores, school_ids):
-                players = [x.find_all('td') for x in box_score_soup]
+        else:
+        
+            if 'var_list' not in set(globals().keys()) & set(locals().keys()):
                 
-                if len(players) < 5:
-                    with open('ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv', 'ab') as hrefscsv:
-                        hrefwriter = csv.writer(hrefscsv, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                        hrefwriter.writerow([game_id,period] + [None]*21)
-                    continue
+                var_list = [x.text for x in
+                            teams[0].find_parent('table').find('tr', {'class': 'grey_heading'}).find_all('th')]
+            
+            for team in teams:
+                
+                school_id = int(re.compile('(?<=team\/)[0-9]+(?=\/)').search(soup.find('a',
+                                text = team.text.strip()).get('href')).group(0))\
+                                if soup.find('a', text = team.text.strip()) != None else None
+                
+                players = [x.find_all('td') for x in team.find_parent('table').find_all('tr', {'class': 'smtext'})]
                 
                 data = OrderedDict()
-                
+                    
                 data['game_id'] = [game_id]*len(players)
                 data['period'] = [period]*len(players)
                 data['school_id'] = [school_id]*len(players)
                 data['order'] = range(len(players))
-                data['player_href'] = [None if x[0].find('a') == None else x[0].find('a').get('href') for x in players]
-                data['player_name'] = [re.sub('[^\x00-\x7F]+','',x[0].text.strip().encode('utf8')) if x[0].has_attr('a') == False
-                                            else x[0].find('a').text for x in players]
-                data['pos'] = [None if x[1].text == '' else x[1].text for x in players]
-                data['min'] = [0 if x[2].text.strip() == '' else 
-                         int(x[2].text.strip().split(':')[0]) + float(x[2].text.strip().split(':')[1])/60 for x in players]
+                data['player_id'] = [int(x[0].find('a').get('href').split('=')[-1]) if x[0].find('a') != None
+                    else None for x in players]
+                data['player_name'] = [re.sub(r'[^\x00-\x7F]+','',x[0].text.strip()) for x in players]
+                data['pos'] = [x[1].text.strip() if x[1].text != '' else None for x in players]
+                data['min'] = [x[2].text.strip() if x[2].text.strip() != '' else None for x in players]
+                
+                for j in range(3,18):
+                    var_name = re.sub(' ','_',var_list[j].lower())
+                    data[var_name] = [int(x[j].text.strip()) if x[j].text.strip() != '' else None for x in players]
+                    
+                if 'box_df' not in set(globals().keys()) & set(locals().keys()):
+                    
+                    box_df = pd.DataFrame(data)
+                    
+                else:
+                    
+                    box_df = pd.concat([box_df, pd.DataFrame(data)])
+                    
+            
+        with open(file_loc, 'ab' if exist else 'wb') as csvfile:
+            box_df.to_csv(csvfile, header = not exist, index = False)
+            
+        exist = True
     
-                for var, j in zip(box_score_var,range(3,18)):
-                    data[var] = [0 if x[j].text.strip() == '' else int(x[j].text.strip()) for x in players]
-                
-                box = pd.DataFrame(data)
-                
-                with open('ncaa_scrapers\\csv\\box_scores_' + str(season) + '.csv', 'ab') as hrefscsv:
-                    box.to_csv(hrefscsv, header = False, index = False)
-        
-        if (i % 100 == 0) or (i == 1):
-            avg_time = (time() - start)/i
-            print(str(season) + ' box_scores Remaining: ' + str(num_needed - i) + ' ' + str(int(avg_time*(num_needed - i)/60)) + ' min')
-        
-        i += 1
+            
+    completed += 1
+
+sys.stdout.flush()
+print('Box Scores: 100.00% Complete')
