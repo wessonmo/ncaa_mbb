0f14c383fa96a4f92be3001e172809e9f3716652
wessonmo <wessonmo@gmail.com>
2018-04-13 01:51:11 -0500

    rewrite and deprecate old code

--- ncaa_scrapers/rosters.py
+++ ncaa_scrapers/rosters.py
@@ -1,59 +1,64 @@
-from ncaa_scrapers.functions.soupify import soupify
-import csv
+from __future__ import print_function
+from functions.soupify import soupify
 import pandas as pd
 from collections import OrderedDict
-from time import time
 import re
-
-school_divs = pd.read_csv('ncaa_scrapers\\csv\\school_divs.csv', header = 0)
-
-for season in range(2012,school_divs.season.max() + 1):
+import os
+import sys
     
-    try:
-        rosters = pd.read_csv('ncaa_scrapers\\csv\\rosters_' + str(season) + '.csv', header = 0)
-    except IOError as error:
-        if str(error) == 'File ncaa_scrapers\\csv\\rosters_' + str(season) + '.csv does not exist':
-            with open('ncaa_scrapers\\csv\\rosters_' + str(season) + '.csv', 'wb') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-                csvwriter.writerow(['season','school_id','jersey','player_id','player_name','pos','height','class'])
-            rosters = pd.read_csv('ncaa_scrapers\\csv\\rosters_' + str(season) + '.csv', header = 0)
-        else:
-            raise error
-            
-    needed = pd.merge(school_divs.loc[school_divs.season == season], rosters, how = 'left', on = ['season','school_id'],
-                      indicator = 'exist')
-    num_needed = len(needed.loc[needed.exist == 'left_only'])
+needed = pd.read_csv('csv\\school_index.csv', header = 0)[['season_id','school_id']]
+needed = set((row.season_id,row.school_id) for index, row in needed[['season_id','school_id']].iterrows())
+
+
+file_loc = 'csv\\rosters.csv'
+
+exist = os.path.isfile(file_loc)
+
+scraped = set() if not exist else set((row.season_id,row.school_id) for index, row in
+              pd.read_csv(file_loc, header = 0)[['season_id','school_id']].drop_duplicates().iterrows())
+
+
+left = needed - scraped
+
+completed = len(scraped)
+for season_id, school_id in left:
     
-    i, start = 1, time()
-    for index, row in needed.loc[needed.exist == 'left_only'].sort_values('school_id').iterrows():
+    sys.stdout.flush()
+    perc_string = '%5.2f'%(float(completed)/len(needed)*100)
+    print('Rosters: {0}% Complete'.format(perc_string), end = '\r')
+    
+    
+    url = 'http://stats.ncaa.org/team/{0}/roster/{1}'.format(school_id, season_id)
+    
+    soup = soupify(url)
+    
+    players = soup.find('th', text = re.compile('Roster')).find_parent('table').find('tbody').find_all('tr')
+    players = [[y.contents[0] if y.contents else '' for y in x.find_all('td')] for x in players]
+    
+    data = OrderedDict()
         
-        soup = soupify('http://stats.ncaa.org/team/' + str(row.school_id) + '/roster/' + str(row.season_id))
-        
-        players = soup.find('table', {'id':'stat_grid'}).find('tbody').find_all('tr')
-        
-        data = OrderedDict()
-        
-        data['season'] = [row.season]*len(players)
-        data['school_id'] = [row.school_id]*len(players)
-        data['jersey'] = [int(re.compile('[0-9]+').search(x.find_all('td')[0].text).group(0))
-                            if re.compile('[0-9]+').search(x.find_all('td')[0].text) else None for x in players]
-        data['player_id'] = [int(x.find_all('td')[1].find('a').get('href').split('=')[-1]) if x.find_all('td')[1].find('a') else None
-                                for x in players]
-        data['player_name'] = [re.sub(r'[^\x00-\x7F]+','',x.find_all('td')[1].text) for x in players]
-        data['pos'] = [x.find_all('td')[2].text if x.find_all('td')[2].text != '' else None for x in players]
-        data['height'] = [x.find_all('td')[3].text if x.find_all('td')[3].text != '' else None  for x in players]
-        data['class'] = [x.find_all('td')[4].text if x.find_all('td')[4].text != '' else None  for x in players]
-        
-        roster = pd.DataFrame(data)
-        
-        if len(roster) == 0:
-            roster.loc[0] = [row.season,row.school_id,None,None,None,None,None,None]
-        
-        with open('ncaa_scrapers\\csv\\rosters_' + str(season) + '.csv', 'ab') as hrefscsv:
-            roster.to_csv(hrefscsv, header = False, index = False)
-            
-        if (i % 100 == 0) or (i == 1):
-            avg_time = (time() - start)/i
-            print(str(season) + ' rosters Remaining: ' + str(num_needed - i) + ' ' + str(int(avg_time*(num_needed - i)/60)) + ' min')
-        
-        i += 1
+    data['season_id'] = [season_id]*len(players)
+    data['school_id'] = [school_id]*len(players)
+    data['jersey'] = [int(x[0]) if re.compile('^[0-9]+$').search(x[0]) else None for x in players]
+    data['player_id'] = [int(x[1].get('href').split('=')[-1]) if x[1].name == 'a' else None for x in players]
+    data['player_name'] = [re.sub(r'[^\x00-\x7F]+','',x[1].text) if x[1].name == 'a'
+        else re.sub(r'[^\x00-\x7F]+','',x[1]) for x in players]
+    data['pos'] = [x[2] if x[2] != '' else None for x in players]
+    data['height'] = [x[3] if x[3] not in ['','-'] else None for x in players]
+    data['class'] = [x[4] if x[4] not in ['','N/A'] else None for x in players]
+    
+    data = pd.DataFrame(data)
+    
+    if len(data) == 0:
+        raise ValueError('no roster: {0}'.format(url))
+    
+    with open(file_loc, 'ab' if exist else 'wb') as csvfile:
+        data.to_csv(csvfile, header = not exist, index = False)
+    
+    exist = True
+    
+    
+    completed += 1
+
+sys.stdout.flush()
+print('Rosters: 100.00% Complete')
