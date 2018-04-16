f14fef23e7ed9771bf04c682c0e9e35ad9c54b6a
wessonmo <wessonmo@gmail.com>
2018-04-16 11:08:27 -0500

    update to include new scrapers with multiprocessing

--- ncaa_scrapers/ncaa_data.py
+++ ncaa_scrapers/ncaa_data.py
@@ -1,36 +1,27 @@
 from __future__ import print_function
 #from sys import argv
-import multiprocessing as mp
+#import multiprocessing as mp
 from scrapers import *
-import time
-import sys
-import pandas as pd
-from __future__ import division
-import sys
+#import time
+#import sys
+#import pandas as pd
+#from __future__ import division
+from sys import argv
+#import tqdm
 
 
 
 if __name__ == '__main__':
     
-    seasons = range(2016,2019)#range(int(argv[1]), int(argv[2]) + 1)
-    divisions = [1]#range(int(argv[3]), int(argv[4]) + 1)
+    seasons = range(int(argv[1]), int(argv[2]) + 1) #range(2016,2019)
+    divisions = range(int(argv[3]), int(argv[4]) + 1) #[1]
     
-    index_loc = 'csv\\school_index.csv'
+    index = school_index.update(seasons, divisions)
     
-    missing_index = school_index.data_check(index_loc, seasons, divisions)
+    rosters.update(index)
+    facilities_coaches_and_schedules.update(index)
+    game_ids = game_ids.update(index)
     
-    if missing_index:
-        
-        pool = mp.Pool()
-        
-        finished = []
-        
-        results = [pool.apply_async(school_index.scrape, args=(x), callback = finished.append) for x in missing_index]
-        
-        while len(finished) < len(missing_index):
-            
-            print('{0}%'.format(float(len(finished))/len(missing_index)))
-            
-            time.sleep(0.5)
-        
-        output = [pd.DataFrame(p.get()).head() for p in results]
+    box_scores.update(game_ids)
+    times_officials_and_pbps.update(game_ids)
+    game_summaries.update(game_ids)
