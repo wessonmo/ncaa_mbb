253b6033bb34ab5789eb9d9cf68cad40a286f3a7
wessonmo <wessonmo@gmail.com>
2018-05-07 23:16:25 -0500

    update file locations

--- rosters/rosters_impute.py
+++ rosters/rosters_impute.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-rosters = pd.read_csv('ncaa_scrapers\\csv\\rosters.csv')
+rosters = pd.read_csv('ncaa_data\\csv\\rosters.csv')
 
 rosters.loc[:,'ncaa_id'] = rosters.player_id.apply(lambda x: True if not pd.isnull(x) else False)
 
@@ -9,5 +9,5 @@ max_id = rosters.loc[rosters.ncaa_id].player_id.max()
 
 rosters.loc[~rosters.ncaa_id,'player_id'] = [x + max_id for x in range(len_miss)]
 
-with open('csv\\imputed_rosters.csv', 'wb') as csvfile:
+with open('rosters\\csv\\imputed_rosters.csv', 'wb') as csvfile:
     rosters.to_csv(csvfile, header = True, index = False)
