431ff61f2210e0b49ae228c3ffcd156cc5eeee51
wessonmo <wessonmo@gmail.com>
2018-05-18 02:03:50 -0500

    move deprecated code out of repo

--- deprecated/scrapers_old/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['school_index',
-           'facilities_coaches_and_schedules',
-           'rosters',
-           'game_ids',
-           'box_scores', 
-           'times_officials_and_pbps',
-           'game_summaries',
-           'index'
-           ]
